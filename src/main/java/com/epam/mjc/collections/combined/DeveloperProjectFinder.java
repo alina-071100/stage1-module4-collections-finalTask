652e037fe9345d508e19df8bf53cfa2244ad3aa2
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-09-20 15:22:56 +0400

    Taks .1. Lessons Getter .2. Map from keys Creator .3. Developer Project Finder

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        return projects.entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream()
+                        .map(v -> new AbstractMap.SimpleEntry<>(v, entry.getKey())))
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue, Collectors.toSet())))
+                .entrySet().stream()
+                .filter(s -> s.getKey().equals(developer))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList())
+                .stream()
+                .flatMap(Collection::stream)
+                .sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()).reversed())
+                .collect(Collectors.toList());
     }
 }
